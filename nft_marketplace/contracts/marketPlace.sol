4fa707216150cd3d65a5bcb79615975fd05a5b82
shock-byte <shuklasr_1@rknec.edu>
2022-02-25 21:38:51 +0530

    two funtions created 1.fetchMarketTokens  2.mintMarketItem

--- nft_marketplace/contracts/marketPlace.sol
+++ nft_marketplace/contracts/marketPlace.sol
@@ -63,6 +63,74 @@ function createMarketSale( address nftContract, uint itemId) public payable nonR
     payable(owner).transfer(listingPrice);
 }
 
+// funtion to fetchMarketItems -minitng, buying and selling
+// returnt the number of unsold items
+
+function fetchMarketTokens() public view returns(MarketToken[] memory) {
+    uint itemCount = _tokenIds.current();
+    uint unsoldItemCount = _tokenIds.current() - _tokensSold.conduct();
+    uint currentIndex = 0;
+
+    //looping over the number of items created(if number has not been sold populated the array)
+    MarketToken[] memory items = new MarketToken[](unsoldItemCount);
+    for(uint i = 0; i < itemCount; i++) {
+        if(idToMarketToken[i + 1].owner == address(0)) {
+            uint currentId = i + 1;
+            MarketToken storage currentItem = idToMarketToken[currentId];
+            items[currentIndex] = currentItem;
+            currentIndex += 1;
+
+        }
+    }
+    return items;
+
+}
+
+function mintMarketItem(
+    address nftContract,
+    uint tokenId,
+    uint price
+)
+public payable nonReentrant {
+
+require(Price > 0, 'Price must be atleast one wei');
+require(msg.value == lisitngPrice, 'Price must be equal to lisiting price');
+
+_tokenIds.increment();
+uint itemId = _tokenIds.current();
+
+idToMarketToken[itemId] = MarketToken(
+    itemId,
+    nftContract,
+    tokenId,
+    payable(msg.sender),
+    payable(address(0)), 
+    price,
+    false
+);
+
+//NFT transaction
+IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+emit MarketTokenMinted(
+    itemId, 
+    nftContract, 
+    tokenId, 
+    msg.sender, 
+    address(0), 
+    price, 
+    false
+    );
+
+//funtions to conduct transactions and market sales
+
+//function to fetchMarketItems
+
+}
+
+
+
+
 
 
 
