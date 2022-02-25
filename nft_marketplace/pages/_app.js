1ec85bd0698fa5687b8c8095727b8b93cd6915e9
Ajinkya Wadekar <ajinkyawadekar0507@gmail.com>
2022-02-25 21:39:26 +0530

    25th feb 2nd commit - Frontend

--- nft_marketplace/pages/_app.js
+++ nft_marketplace/pages/_app.js
@@ -1,8 +1,37 @@
 import '../styles/globals.css'
-
 import Link from 'next/link'
 
-function NFTmarketplace({Compenent, pageProps}) {
-  return  {
-    <div> </      }
+function NFTmarketplace({Component, pageProps}){
+  return (
+    <div>
+      <nav className='border-b p-6' style={{backgroundColor:'brown'}}>
+        <p className='text-4x1 font-bold test-white'>NFT-Marketplace</p>
+        <div className='flex mt-4 justify-center'>
+          <Link href='/'>
+            <a className='mr-4'>
+              Main Marketplace
+            </a>
+          </Link>
+          <Link href='/mint-item'>
+            <a className='mr-6'>
+              Mint tokens
+            </a>
+          </Link>
+          <Link href='/my-nfts'>
+            <a className='mr-6'>
+              My NFTs
+            </a>
+          </Link>
+          <Link href='/account-dashboard'>
+            <a className='mr-6'>
+              Account Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
+
+export default NFTmarketplace
