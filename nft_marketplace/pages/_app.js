889df896ec828812abfec3bb6f76f9e3afd5400f
ashpardhi9696 <ashpardhi9696@gmail.com>
2022-04-20 19:47:30 +0530

    front page added

--- nft_marketplace/pages/_app.js
+++ nft_marketplace/pages/_app.js
@@ -1,43 +1,87 @@
-import '../styles/globals.css'
-import './app.css'
-import Link from 'next/link'
-
-
-function NFTmarketplace({Component, pageProps}) {
+import "../styles/globals.css";
+import "./app.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+function NFTmarketplace({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <div>
-      <nav className='border-b p-6' style={{backgroundColor:'#00539CFF'}}>
-        <p className='text-4x1 font-bold text-orange-300 text-center pt-3'>ManiacNFT </p>
-        
-        <div className='flex mt-4 justify-center'>
-          <Link href='/'>
-            <a className='mr-4 text-orange-200 no-underline font-semibold'>
-              Home 
+      <div>
+        <nav className="z-10 w-full shadow-sm ">
+          <div className="flex items-center w-full h-20">
+            <div className="flex items-center justify-between w-full mx-20">
+              <div className="flex items-center justify-center flex-shrink-0 ">
+                <h1 className="text-xl font-bold ">
+                  Maniac<span className="text-blue-500">NFT</span>
+                </h1>
+              </div>
+              <div className="hidden md:block">
+                <div className="flex items-baseline ml-10 space-x-4">
+                  <Link href="/">
+                    <a className={router.pathname == "/" ? "active" : ""}>
+                      Home
+                    </a>
+                  </Link>
+                  <Link
+                    href="/Marketplace"
+                    activeClass="about"
+                    className="px-3 py-2 text-sm font-medium text-black rounded-md hover:bg-blue-600 hover:text-white"
+                  >
+                    MarketPlace
+                  </Link>
+                  <Link
+                    href="/my-nfts"
+                    activeClass="Services"
+                    className="px-3 py-2 text-sm font-medium text-black rounded-md hover:bg-blue-600 hover:text-white"
+                  >
+                    My NFts
+                  </Link>
+
+                  <Link
+                    activeClass="contact"
+                    href="/creator-dashboard"
+                    className="px-3 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-black"
+                  >
+                    Creator Dashboard
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      {/* <Nav /> */}
+      {/* <nav className="p-6 border-b" style={{ backgroundColor: "#00539CFF" }}>
+        <p className="pt-3 font-bold text-center text-orange-300 text-4x1">
+          ManiacNFT{" "}
+        </p>
+
+        <div className="flex justify-center mt-4">
+          <Link href="/">
+            <a className="mr-4 font-semibold text-orange-200 no-underline">
+              Home
             </a>
           </Link>
-          <Link href='/Marketplace'>
-            <a className='mr-6 text-orange-200 no-underline font-semibold '>
+          <Link href="/Marketplace">
+            <a className="mr-6 font-semibold text-orange-200 no-underline ">
               Marketplace
             </a>
           </Link>
-          <Link href='/my-nfts'>
-            <a className='mr-6 text-orange-200 no-underline font-semibold '>
+          <Link href="/my-nfts">
+            <a className="mr-6 font-semibold text-orange-200 no-underline ">
               My NFTs
             </a>
           </Link>
-          <Link href='/creator-dashboard'>
-            <a className='mr-6 text-orange-200 no-underline font-semibold'>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 font-semibold text-orange-200 no-underline">
               Creator Dashboard
             </a>
           </Link>
         </div>
-      </nav> 
-
+      </nav> */}
       <Component {...pageProps} />
     </div>
-
-  )
+  );
 }
 
-
-export default NFTmarketplace
+export default NFTmarketplace;
--- nft_marketplace/pages/index.js
+++ nft_marketplace/pages/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-
+import React from "react";
 export default function Home() {
-  const images = ['https://images.unsplash.com/photo-1506501139174-099022df5260?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80', 'https://images.unsplash.com/photo-1523438097201-512ae7d59c44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80', 'https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80']
+  const images = [
+    "https://images.unsplash.com/photo-1506501139174-099022df5260?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80",
+    "https://images.unsplash.com/photo-1523438097201-512ae7d59c44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+  ];
 
   // We will start by storing the index of the current image in the state.
   const [currentImage, setCurrentImage] = React.useState(0);
@@ -14,7 +17,7 @@ export default function Home() {
     return acc;
   }, {});
 
-  const scrollToImage = i => {
+  const scrollToImage = (i) => {
     // First let's set the index of the image we want to see next
     setCurrentImage(i);
     // Now, this is where the magic happens. We 'tagged' each one of the images with a ref,
@@ -23,11 +26,11 @@ export default function Home() {
     // image's ref in 'refs' array above.
     refs[i].current.scrollIntoView({
       //     Defines the transition animation.
-      behavior: 'smooth',
+      behavior: "smooth",
       //      Defines vertical alignment.
-      block: 'nearest',
+      block: "nearest",
       //      Defines horizontal alignment.
-      inline: 'start',
+      inline: "start",
     });
   };
 
@@ -54,73 +57,233 @@ export default function Home() {
 
   // Tailwind styles. Most importantly notice position absolute, this will sit relative to the carousel's outer div.
   const arrowStyle =
-    'absolute text-white text-2xl z-10 bg-black h-10 w-10 rounded-full opacity-75 flex items-center justify-center';
+    "absolute text-white text-2xl z-10 bg-black h-10 w-10 rounded-full opacity-75 flex items-center justify-center";
 
   // Let's create dynamic buttons. It can be either left or right. Using
   // isLeft boolean we can determine which side we'll be rendering our button
   // as well as change its position and content.
-  const sliderControl = isLeft => (
+  const sliderControl = (isLeft) => (
     <button
       type="button"
       onClick={isLeft ? previousImage : nextImage}
-      className={`${arrowStyle} ${isLeft ? 'left-2' : 'right-2'}`}
-      style={{ top: '40%' }}
+      className={`${arrowStyle} ${isLeft ? "left-2" : "right-2"}`}
+      style={{ top: "40%" }}
     >
-      <span role="img" aria-label={`Arrow ${isLeft ? 'left' : 'right'}`}>
-        {isLeft ? '◀' : '▶'}
+      <span role="img" aria-label={`Arrow ${isLeft ? "left" : "right"}`}>
+        {isLeft ? "◀" : "▶"}
       </span>
     </button>
   );
-  
+
   return (
-    
-
-    <div className=" px-7 py-5">
-
-<div className="p-12 flex justify-center w-screen md:w-1/2 items-center">
-      <div className="relative w-full">
-        <div className="carousel inline-flex overflow-x-hidden" styles={{
-          'scroll-snap-type': 'x mandatory'
-        }}>
-          {sliderControl(true)}
-          {images.map((img, i) => (
-            <div className="w-full flex-shrink-0" key={img} ref={refs[i]}>
-              <img src={img} className="w-full object-contain" />
-            </div>
-          ))}
-          {sliderControl()}
+    <div className="py-5 ">
+      <div className="flex items-center justify-center w-full p-12 max-h-1/4">
+        <div className="relative w-full">
+          <div
+            className="inline-flex overflow-x-hidden carousel"
+            styles={{
+              "scroll-snap-type": "x mandatory",
+            }}
+          >
+            {sliderControl(true)}
+            {images.map((img, i) => (
+              <div className="flex-shrink-0 w-full" key={img} ref={refs[i]}>
+                <img src={img} className="object-contain w-full" />
+              </div>
+            ))}
+            {sliderControl()}
+          </div>
         </div>
       </div>
-    </div>
 
-      <div className="px-7 py-5 text-orange-200 bg-sky-700">
+      <div className="py-5 px-7 ">
         <p className="text-4xl">What are NFTs</p>
-        <p className="text-xl py-4">
-        NFTs or Non-Fungible Tokens are digital tokens that represent ownership of any unique asset like art, collectibles, music, popular moments of history, even real estate. At a time, an NFT can have only one official owner in the world as it is stored and secured using Blockchain technology, which means no one can modify the ownership of an NFT which exists. The ownership can be sold to another person and only then it will be reflected on Blockchain.
-        "Non-Fungible" means something which is unique, which is only one of its kind and which is not interchangeable. For example, a limited- edition Football player collectible card which is only one in the entire world and which can’t be exchanged for another one because similar card doesn’t exist.
-        Whereas, "Fungible" means something which can be exchanged for another similar entity, something which isn’t unique. Like a 10 rupees note can be exchanged for another 10 rupees note or a bitcoin which can be traded for another bitcoin. 
-
-        One can buy, sell, trade, or give NFTs to anyone online, but a network of computers must verify the trade and the unique data. NFTs are secured with a unique code assigned with it for authenticity purposes. Customers or fans especially the die-hard loyal ones are willing to pay for the experiences and values and their icons they believe in. Brands can see this as an opportunity by creating unique assets like a exclusive special message for a person on his/her birthday, or any advertisement poster signed by its brand ambassador, collectible digital cards,memoribilia,etc to build stronger relationships with their fanbase. The values of such assets may appreciate over time.
-
+        <p className="py-4 text-xl">
+          NFTs or Non-Fungible Tokens are digital tokens that represent
+          ownership of any unique asset like art, collectibles, music, popular
+          moments of history, even real estate. At a time, an NFT can have only
+          one official owner in the world as it is stored and secured using
+          Blockchain technology, which means no one can modify the ownership of
+          an NFT which exists. The ownership can be sold to another person and
+          only then it will be reflected on Blockchain. "Non-Fungible" means
+          something which is unique, which is only one of its kind and which is
+          not interchangeable. For example, a limited- edition Football player
+          collectible card which is only one in the entire world and which can’t
+          be exchanged for another one because similar card doesn’t exist.
+          Whereas, "Fungible" means something which can be exchanged for another
+          similar entity, something which isn’t unique. Like a 10 rupees note
+          can be exchanged for another 10 rupees note or a bitcoin which can be
+          traded for another bitcoin. One can buy, sell, trade, or give NFTs to
+          anyone online, but a network of computers must verify the trade and
+          the unique data. NFTs are secured with a unique code assigned with it
+          for authenticity purposes. Customers or fans especially the die-hard
+          loyal ones are willing to pay for the experiences and values and their
+          icons they believe in. Brands can see this as an opportunity by
+          creating unique assets like a exclusive special message for a person
+          on his/her birthday, or any advertisement poster signed by its brand
+          ambassador, collectible digital cards,memoribilia,etc to build
+          stronger relationships with their fanbase. The values of such assets
+          may appreciate over time.
         </p>
       </div>
 
+      <div className="my-10 text-4xl text-center">
+        Resources For Getting Started
+      </div>
+      <div className="flex flex-wrap justify-around">
+        <div class="flex justify-center">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm  hover:shadow-2xl">
+            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+              <img
+                class="rounded-t-lg"
+                src="https://opensea.io/blog/wp-content/uploads/2022/02/image-13.png"
+                alt=""
+              />
+            </a>
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">
+                Setting Up Metamask Wallet
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-center">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm  hover:shadow-2xl">
+            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+              <img
+                class="rounded-t-lg"
+                src="https://opensea.io/blog/wp-content/uploads/2021/02/creating-selling-nfts.png"
+                alt=""
+              />
+            </a>
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">
+                Funding Your Metamask Wallet
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="  flex justify-center hover:shadow-2xl">
+          <div class="rounded-lg shadow-xl bg-white max-w-sm">
+            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+              <img
+                class="rounded-t-lg"
+                src="https://opensea.io/blog/wp-content/uploads/2022/02/image-13.png"
+                alt=""
+              />
+            </a>
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <div className=" flex justify-around py-10 text-orange-200 ">
-        <div className="px-3 py-3 w-1/4 bg-red-500"> 
-        <p className="text-2xl">Connect your Wallet</p>
-        <p className="text-lg py-3">Download the Metamask Extension for chrome and login for facilitating transactions on our NFT MarketPlace</p>
+      {/* <div className="flex justify-center py-10 text-orange-200 ">
+        <div className="w-1/4 px-3 py-3 bg-red-500">
+          <p className="text-2xl">Connect your Wallet</p>
+          <p className="py-3 text-lg">
+            Download the Metamask Extension for chrome and login for
+            facilitating transactions on our NFT MarketPlace
+          </p>
         </div>
-        <div className="px-3 py-3 w-1/4 bg-green-500"> 
-        <p className="text-2xl">create NFT</p>
-        <p className="text-lg py-3">Easily create NFTs,add description and set price for you artwork through  "Mint NFTs" Tab  </p>
+
+        <div>
+          <div className="px-3 py-3 my-4 bg-green-500 ">
+            <p className="text-2xl">create NFT</p>
+            <p className="py-3 text-lg">
+              Easily create NFTs,add description and set price for you artwork
+              through "Mint NFTs" Tab{" "}
+            </p>
+          </div>
+          <div className="px-3 py-3 bg-orange-500 ">
+            <p className="text-2xl">easily sell NFT</p>
+            <p className="py-3 text-lg">
+              Easy,safe and secure transactions and the amount gets directly
+              deposited in your Metamask wallet .
+            </p>
+          </div>
         </div>
-        <div className="px-3 py-3 w-1/4 bg-orange-500">
-        <p className="text-2xl">easily sell NFT</p>
-        <p className="text-lg py-3">Easy,safe and secure transactions and the amount gets directly deposited in your Metamask wallet .</p>
+      </div> */}
+
+      <div className="flex px-10 py-10 my-10 bg-blue-50 justify-evenly">
+        <div class="flex justify-center ">
+          <div class=" rounded-lg   max-w-sm ">
+            <img
+              class="rounded-t-lg px-20"
+              src="https://opensea.io/static/images/icons/wallet.svg"
+              alt=""
+            />
+
+            <div class="p-6 ">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">
+                Connect Your Wallet
+              </h5>
+              <p class="text-gray-700 text-base mb-4">
+                Download the Metamask Extension for chrome and login for
+                facilitating transactions on our NFT MarketPlace
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex justify-center ">
+          <div class="rounded-lg  max-w-sm">
+            <img
+              class="rounded-t-lg px-20"
+              src="https://opensea.io/static/images/icons/nft.svg"
+              alt=""
+            />
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">Create NFT</h5>
+              <p class="text-gray-700 text-base mb-4">
+                Upload your work, add a title and description, and customize
+                your NFTs.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex justify-center">
+          <div class="rounded-lg  max-w-sm">
+            <img
+              class="rounded-t-lg px-20"
+              src="https://opensea.io/static/images/icons/collection.svg"
+              alt=""
+            />
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">
+                Create your collection
+              </h5>
+              <p class="text-gray-700 text-base mb-4">
+                Click{" "}
+                <a href="/marketplace" className="text-blue-800 ">
+                  <span className="font-bold">My Collections</span>
+                </a>{" "}
+                and set up your collection. Add a description, profile and
+                banner images, and set a secondary sales fee.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex justify-center">
+          <div class="rounded-lg  max-w-sm">
+            <img
+              class="rounded-t-lg px-20"
+              src="https://opensea.io/static/images/icons/sale.svg"
+              alt=""
+            />
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">Trade NFTs</h5>
+              <p class="text-gray-700 text-base mb-4 styles.centerAlign">
+                Easy,safe and secure transactions and the amount gets directly
+                deposited in your Metamask wallet .
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
-  )
+  );
 }
